501eba5e3c8816b388ad56c2a437d95e2d1e277d
Ryan Howard <ryanghoward@protonmail.com>
2022-02-14 20:12:14 -0800

    SQL queries added pt 2

--- queries.sql
+++ queries.sql
@@ -24,8 +24,6 @@ insert into Customers (CustomerName, ContactName, Address, City, PostalCode, Cou
 
 update Customers set PostalCode = '11122' where PostalCode = '111';
 
-
-
 -- (Stretch) Find a query to discover how many different cities are stored in the Customers table. Repeats should not be double counted
 
 -- (Stretch) Find all suppliers who have names longer than 20 characters. You can use `length(SupplierName)` to get the length of the name
