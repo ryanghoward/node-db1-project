f4d709881dbe5421ce9fd56c5d9e166d3247fd77
Ryan Howard <ryanghoward@protonmail.com>
2022-02-14 16:56:06 -0800

    Twelfth commit

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -21,7 +21,10 @@ router.post(
   middleware.checkAccountNameUnique,
   async (req, res, next) => {
     try {
-      const newAccount = await Account.create(req.body);
+      const newAccount = await Account.create({
+        name: req.body.name.trim(),
+        budget: req.body.budget,
+      });
       res.status(201).json(newAccount);
     } catch (err) {
       next(err);
