4e35d3b7c9ff3330fae8945c7d083ef817e7f75b
Ryan Howard <ryanghoward@protonmail.com>
2022-02-14 16:21:02 -0800

    Eighth commit

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,4 +1,5 @@
 const Account = require("./accounts-model");
+const db = require("../../data/db-config");
 
 exports.checkAccountPayload = (req, res, next) => {
   const error = { status: 400 };
@@ -22,9 +23,20 @@ exports.checkAccountPayload = (req, res, next) => {
   }
 };
 
-exports.checkAccountNameUnique = (req, res, next) => {
-  console.log("checkAccountNameUnique middleware");
-  next();
+exports.checkAccountNameUnique = async (req, res, next) => {
+  try {
+    const existing = await db("accounts")
+      .where("name", req.body.name.trim())
+      .first();
+
+    if (existing) {
+      next({ status: 400, message: "that name is already in use" });
+    } else {
+      next();
+    }
+  } catch (err) {
+    next(err);
+  }
 };
 
 exports.checkAccountId = async (req, res, next) => {
